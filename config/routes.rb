62e82fcdf0329ae75061ffbcfb3354551f40c8b9
clemkeirua <clement@keiruaprod.fr>
2021-03-16 11:18:44 +0100

    delete everything, oh yeah🤘

--- config/routes.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-Discourse::Application.routes.draw do
-  match '/*path', to: redirect { |params, request| "http://othersite.com/#{params[:path]}" }, via: [:get, :post]
-end
